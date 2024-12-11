706e1c6b4947aae71172c3f15956501976c0079b
Fox Sadik <fox.sadik@gmail.com>
2024-12-11 22:58:09 +0100

    Add image upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wxbwxygdxhheawskcaej.supabase.co";
+export const supabaseUrl = "https://wxbwxygdxhheawskcaej.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4Ynd4eWdkeGhoZWF3c2tjYWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxODM4NDQsImV4cCI6MjA0ODc1OTg0NH0.g4Hhvues-nkJ52j-CB2-D831PyttVMFUCDaPU4X6A2A";
 const supabase = createClient(supabaseUrl, supabaseKey);
